5d542a8e3880e57a79968784033d196ed725d454
maratiik <maratpeople@gmail.com>
2023-10-12 23:29:13 +0300

    Update valo_checker.py
    Table will be printed only if changed.

--- valo_checker.py
+++ valo_checker.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import time
 
+old_valorant_table = pd.DataFrame()
+
 while(True):
     tables = pd.read_html('https://docs.google.com/spreadsheets/d/1lmuMYYLpJENk6dho7-l0jmPsHoVVl2c9jpaQzRW0SiQ/htmlview#', header = 1, keep_default_na=False)
-
     # Finding only Valorant table
     for table in tables:
         if table.columns.values[1] == 'Valorant':
@@ -18,11 +19,19 @@ while(True):
         if (type(valorant_table['Region'][row]) is str):
             valorant_table['Region'][row] = valorant_table['Region'][row].upper()
 
-    # Show only EU cases
+    # Filtering only EU cases
     valorant_table = valorant_table[valorant_table['Region'].str.contains('EU')]
-    if valorant_table.empty:
-        print('\n' * 30, 'No EU cases for now')
-    else:
-        print('\n' * 30, valorant_table)
 
-    time.sleep(6)
+    # Checking if the table somehow changed from the last time
+    if old_valorant_table.equals(valorant_table):
+        pass
+    else:
+        if valorant_table.empty:
+            print('\n' * 30, 'No EU cases for now')
+        else:
+            print('\n' * 30, valorant_table)
+
+    # Creating a deep copy of the table to compare them in the if statement
+    old_valorant_table = valorant_table.copy(deep=True)
+
+    time.sleep(5)
